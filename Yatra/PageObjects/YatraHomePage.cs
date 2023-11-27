5f7ccc30b65913257edad569fabd25eedf4c0b61
maria.baugustine <maria.baugustine@ust.com>
2023-11-28 00:06:47 +0530

    gdgefd

---
+++ Yatra/PageObjects/YatraHomePage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yatra.PageObjects
+{
+    internal class YatraHomePage
+    {
+        IWebDriver driver;
+
+        public YatraHomePage(IWebDriver driver)
+        {
+            this.driver = driver??throw new ArgumentException(nameof(driver));
+            PageFactory.InitElements(driver, this);
+        }
+        [FindsBy(How =How.Id,Using = "booking_engine_homestays")]
+        private IWebElement villasIcon {  get; set; }
+        public void ClickVillasIcon()
+        {
+            villasIcon.Click();
+        }
+    }
+}
---
+++ Yatra/TestScripts/VillasBookingTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yatra.PageObjects;
+using Yatra.Utilities;
+
+namespace Yatra.TestScripts
+{
+    internal class VillasBookingTests:CoreCodes
+    {
+        [Test]
+        public void TestvillasBooking()
+        {
+            var yatraHP = new YatraHomePage(driver);
+            yatraHP.ClickVillasIcon();
+            Assert.AreEqual("https://www.yatra.com/homestays", driver.Url);
+        }
+    }
+}
